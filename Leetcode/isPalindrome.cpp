4f694ff37c20b3bb33222acd1910301cc60c3c56
Shaurya <shauryashekhar18@gmail.com>
2020-03-23 18:44:36 -0500

    3/23

---
+++ Leetcode/isPalindrome.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        string s = to_string(x);
+        int l = 0;
+        int r = s.size() - 1;
+        while(l < r) {
+            if(s[l] == s[r]) {
+                l++;
+                r--;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+};
